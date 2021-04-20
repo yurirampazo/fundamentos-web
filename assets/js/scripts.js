431476f5092ddfbace3280ce4e41677641d60437
Yuri <yuri.rampazo@outlook.com>
2021-04-20 14:42:25 -0300

    Added DOM

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,7 +16,10 @@ por seletor: querrySelector()
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
-
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 nome.style.width = '100%'
 email.style.width = '100%'
 
@@ -43,3 +46,34 @@ function validaEmail() {
         emailOk = true
     }
 }
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+ 
+    if (assunto.value.length >= 2) {
+       txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+       txtAssunto.style.color = 'red'
+       txtAssunto.style.display = 'block'
+    } else {
+       txtAssunto.style.display = 'none'
+       assuntoOk = true
+    }
+ }
+
+ function enviar() {
+     if (nomeOk == true && emailOk == true && assuntoOk == true) {
+         alert('Formulário preechido com sucesso')
+     } else {
+         alert ('Preencha o formulário corretamente antes de enviar')
+     }
+ }
+
+ function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+ }
+
+ function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+ }
